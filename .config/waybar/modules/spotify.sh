687fdc51c9d8da7d2d8fa9df878f7ad68889bb91
AirvinJames <airvinjames0216@gmail.com>
2025-12-25 20:59:31 +0800

    late final rice for 2025

--- .config/waybar/modules/spotify.sh
+++ .config/waybar/modules/spotify.sh
@@ -1,18 +1,20 @@
 #!/bin/sh
 
 class=$(playerctl metadata --format '{{lc(status)}}')
-icon=""
+icon=""
+iconOff="󰝛"
 
 if [[ $class == "playing" ]]; then
   info=$(playerctl metadata --format '{{artist}} - {{title}}')
   if [[ ${#info} > 40 ]]; then
     info=$(echo $info | cut -c1-40)"..."
   fi
-  text=$info" "$icon
+  text=$icon"    "$info
 elif [[ $class == "paused" ]]; then
-  text=$icon
-elif [[ $class == "stopped" ]]; then
-  text=$icon" Offline"
+  text=$icon" Paused"
+elif [[ $class == "" ]]; then
+  text=$iconOff" Nothing playing..."
 fi
 
 echo -e "{\"text\":\""$text"\", \"class\":\""$class"\"}"
+
